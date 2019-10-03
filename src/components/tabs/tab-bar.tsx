be720c1bf31c5062c24df6ecedbced01d674f93c
Gustavo Freire Santos <gustavo.freire.s@outlook.com>
2019-10-03 15:09:44 -0300

    only verify by active prop12

--- src/components/tabs/tab-bar.tsx
+++ src/components/tabs/tab-bar.tsx
@@ -346,7 +346,7 @@ const TabBar = (props: TabBarProps) => {
           return (
             <div
               id={`${child.id}-panel`}
-              // key={`${child.id}-panel`}
+              key={`${child.id}-panel`}
               className={`tab-panel ${
                 checkActive(child) && !props.hiddenPanel ? 'active' : ''
               }`}
