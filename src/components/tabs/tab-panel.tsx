068ef6a2c3c2e455e5180cc51d5ff2276aac4ba6
Gustavo Freire Santos <gustavo.freire.s@outlook.com>
2019-10-22 14:29:32 -0300

    added react memo feature2

--- src/components/tabs/tab-panel.tsx
+++ src/components/tabs/tab-panel.tsx
@@ -8,7 +8,7 @@ const areEqual = (prevProps: any, nextProps: any) => {
   return true;
 };
 const TabPanel = React.memo((props: Props) => {
-  console.log('render');
+  console.log('tabPanel render');
   return <div>{props.children}</div>;
 }, areEqual);
 
